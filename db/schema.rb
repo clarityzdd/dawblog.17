1da58763a8272b8aead69eb567ab21f673700477
root <root@DAW-Machine.(none)>
2017-11-30 12:29:06 +0000

    fin activerecord1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124115415) do
+ActiveRecord::Schema.define(version: 20171130120134) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,23 @@ ActiveRecord::Schema.define(version: 20171124115415) do
     t.datetime "updated_at", null: false
     t.string "excerpt"
     t.string "location"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.date "birthday"
+    t.text "bio"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
