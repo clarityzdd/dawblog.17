639b415daadeca7557e508c060658624001546e0
root <root@DAW-Machine.(none)>
2017-12-01 12:12:23 +0000

    con relaciones

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create email: "test@test.com", password: 'test'
+categories = Category.create [
+  {name: 'Programming'},
+  {name: 'TV'},
+  {name: 'Music'},
+  {name: 'Travel'},
+  {name: 'other'}
+  ]
+articles = Article.create [
+    {title: "Omega Rom", body: "mejor custom rom para android", published_at: '2014-05-20'},
+    {title: "Custom Rom", body: "personaliza tu android", published_at: Time.now},
+    {title: "xda-developers", body: "foro de freaks", published_at: 20.months.ago},
+    {title: "Borrador", body: "articulo en desarrollo"}
+  ]  
+user.articles << articles
+user.articles.find(1).categories << Category.find_by_name('Programming')
+
+article = user.articles.first
+article.comments.create name:"invitado", email:"invitado@test.com", body: "artículo estupendo"
+article = user.articles.last
+2.times {|n| article.comments.create! name: "invitado #{n}", email: "invitado#{n}@test.com", body: "muy bueno invitado #{n}"}
+
+  
